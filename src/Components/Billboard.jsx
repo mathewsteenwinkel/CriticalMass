4d251e6500731c29f5434a0b82904e79909406f7
Noidea22 <mathewsteenwinkel@gmail.com>
2025-01-29 12:24:05 -0700

    started billboard

---
+++ src/Components/Billboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Billboard.css'
+
+
+function Billboard() {
+  return (
+    <div className='header-container'>
+      <a>
+        <div className='name'> CriticalMass
+        <div className='period'>.</div>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export default Billboard
