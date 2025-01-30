d2d83695e3c4a1a80fd6dc3359fad803975d7e9e
Noidea22 <mathewsteenwinkel@gmail.com>
2025-01-30 12:56:59 -0700

    billboard design built without motion

--- src/Components/Billboard.jsx
+++ src/Components/Billboard.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Billboard.css'
+import Leaf from '../Assets/Mapleleaf.png'
 
 
 function Billboard() {
   return (
     <div className='header-container'>
       <a>
-        <div className='name'> CriticalMass
-        <div className='period'>.</div>
+        <div className='name'> Critical Mass.
+          <div className='subHeading'>
+            <div className='left-side'>
+              Welcome.
+              <div>Digital is our home.</div>
+            </div>
+            <div className='right-side'>
+                <img src={Leaf} className='leaf'/> 
+              <div className='wordings-right'>
+              Canada + the World
+                <div>
+                Since '96
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </a>
     </div>
