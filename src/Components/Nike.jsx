a5ccf2f4afee2f6ee831dd68f9cffdc668565a9b
Mathew Steenwinkel <mathewsteenwinkel@gmail.com>
2025-02-05 19:32:40 -0700

    added subheading to nike

--- src/Components/Nike.jsx
+++ src/Components/Nike.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Nike.css'
+import NikeText from './NikeText'
 
 function Nike() {
   return (
-    <div className='nike-container'>
-      <div className='nike-content'>
-      <img src='./Assets/Nike.jpg' alt="Nike" className="nike" />
+    <>
+      <div className='nike-container'>
+        <div className='nike-header'>
+          <div className='nike-content'>
+            <img src='./Assets/Nike.jpg' alt="Nike" className="nike" />
+            <video className="nike-video nike" autoPlay loop muted >
+              <source src='./Assets/DoWhatYouCant.mp4' type='video/mp4' />
+            </video>
+          </div>
+          <NikeText />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
---
+++ src/Components/NikeText.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NikeText.css'
+
+function NikeText() {
+  return (
+    <div>
+      <div className='nike-text'>
+        <div className='nike-title'>Nike. </div>
+        <div className='nike-subheading'>Changing the Game Together.</div>
+        <div className='nike-description'>
+          STRATEGY / BRAND & CAMPAIGN / ARTIFICIAL INTELLIGENCE / CONTENT PRODUCTION
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NikeText
