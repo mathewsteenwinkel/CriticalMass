b6791d9edfb80803cb226c1f851b957c0c88c2e1
Mathew Steenwinkel <mathewsteenwinkel@gmail.com>
2025-02-07 16:14:55 -0700

    blog changes made to layout and images

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -6,7 +6,6 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-// import required modules
 import { FreeMode, Pagination } from 'swiper/modules';
 
 export default function Articles() {
@@ -28,10 +27,10 @@ export default function Articles() {
       >
         <SwiperSlide className='slide'>
           <div className='photo-div'>
-          <img src='./Assets/adobestock_892449730.jpeg.avif' className='photo' />
+            <img src='./Assets/adobestock_892449730.jpeg.avif' className='photo' />
           </div>
           <div className='Name-Date'>
-            <p className='author'> John Doe</p>
+            <p className='author'> Jennifer faull</p>
             <p className='date'>18.10.2024</p>
           </div>
           <div className='body-text'>
@@ -40,30 +39,53 @@ export default function Articles() {
         </SwiperSlide>
         <SwiperSlide className='slide'>
           <div className='photo-div'>
-          <img src='./Assets/adobestock_892449730.jpeg.avif' className='photo' />
+            <img src='./Assets/Handshake.jpg' className='photo' />
           </div>
           <div className='Name-Date'>
-            <p className='author'> John Doe</p>
-            <p className='date'>18.10.2024</p>
+            <p className='author'> Dan Murphy</p>
+            <p className='date'>21.08.2024</p>
           </div>
           <div className='body-text'>
-            Agency Leaders Share Their Five-Year Plans for Future-Proofing
+            B2B with a Human Heart: How Digital Experiences Can Show Buyers You Understand Them
           </div>
         </SwiperSlide>
         <SwiperSlide className='slide'>
           <div className='photo-div'>
-          <img src='./Assets/adobestock_892449730.jpeg.avif' className='photo' />
+            <img src='./Assets/Cerave.jpg' className='photo' />
           </div>
           <div className='Name-Date'>
-            <p className='author'> John Doe</p>
-            <p className='date'>18.10.2024</p>
+            <p className='author'> Bailey Calfee</p>
+            <p className='date'>27.06.2024</p>
           </div>
           <div className='body-text'>
-            Agency Leaders Share Their Five-Year Plans for Future-Proofing
+            Winning Cannes Work Put the Focus on Commercial Creativity
           </div>
         </SwiperSlide>
-       
-      
+        <SwiperSlide className='slide'>
+          <div className='photo-div'>
+            <img src='./Assets/Starburst_Possibilities.png' className='photo' />
+          </div>
+          <div className='Name-Date'>
+            <p className='author'> Tim Nudd</p>
+            <p className='date'>07.05.2024</p>
+          </div>
+          <div className='body-text'>
+            Starbust Jumps Into Generative AI With 'Different Every Time' Brand Campaign
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className='slide'>
+          <div className='photo-div'>
+            <img src='./Assets/MandM.webp' className='photo' />
+          </div>
+          <div className='Name-Date'>
+            <p className='author'> </p>
+            <p className='date'>28.05.2024</p>
+          </div>
+          <div className='body-text'>
+            Mars and Omnicon on Embracing Generative AI: 'This is a wild West Tool'
+          </div>
+        </SwiperSlide>
+
 
 
       </Swiper>
