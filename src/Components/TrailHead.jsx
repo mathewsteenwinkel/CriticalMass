086317136548ddd5ab1a82da0bc51a1fb1a19cca
Mathew Steenwinkel <mathewsteenwinkel@gmail.com>
2025-02-11 15:41:12 -0700

    traiLhead section complete

--- src/Components/TrailHead.jsx
+++ src/Components/TrailHead.jsx
@@ -8,7 +8,9 @@ import TrailHeadCursor from './TrailHeadCursor'
 function TrailHead() {
   const [cursorVisible, setCursorVisible] = useState(false);
 
-
+  const text = <>
+    Our in-house development <br /> program for emerging talent.
+  </>
 
   return (
     <div className='trailhead-div'>
@@ -19,11 +21,23 @@ function TrailHead() {
         onMouseLeave={() => setCursorVisible(false)}
       >
         <img src='./Assets/texture.jpg' className='texture' />
-        <div className='Trail-right'>
-
+        <div className='Trail-left'>
+          <div className='left-div'>
+            <div className='Trail-info'>
+              {text}
+            </div>
+            <div className='Trail-logo'>
+              <img src='./Assets/Tralhead_logo_blue.png' className='Trail-logo' />
+            </div>
+            <div className='Trail-info'>
+              No experience required
+            </div>
+          </div>
         </div>
-        <div className='Trail-Left'>
-
+        <div className='Trail-right'>
+          <div className='photo'>
+            <img src='./Assets/Me.png' className='photo-me' />
+          </div>
         </div>
       </div>
     </div>
