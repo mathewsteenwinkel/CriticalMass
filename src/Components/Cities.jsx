262c3f65067c86093de77ad3749991677f17b0fe
Mathew Steenwinkel <mathewsteenwinkel@gmail.com>
2025-02-13 15:09:37 -0700

    all cities added

--- src/Components/Cities.jsx
+++ src/Components/Cities.jsx
@@ -4,7 +4,7 @@ import './Cities.css'
 function Cities() {
   return (
     <div className='Cities-div'>
-      <div className='Citie-container'>
+      <div className='City-container'>
         <div className='Seperator' />
         <div className='City'>
           <div className='City-name'>
@@ -21,8 +21,286 @@ function Cities() {
           </div>
           <div className='location-jobs'>
             <div className='location-opening'>
+              7
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Chicago, IL
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>225 N Michigan Ave</p>
+              <p className='address-street'> Suite 700</p>
+              <p className='address-street'> Chicago, IL</p>
+              <p className='address-street'> 60601, USA</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              7
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Cupertino, CA
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>75 E Santa Clara St</p>
+              <p className='address-street'>7th Floor</p>
+              <p className='address-street'>San Jose, CA</p>
+              <p className='address-street'> 95113, USA</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              1
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            London, GB
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>100 Southwark Street </p>
+              <p className='address-street'> Bankside 2</p>
+              <p className='address-street'> London, GB</p>
+              <p className='address-street'> SE1 0SW, UK</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              6
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Los Angeles, CA
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>5353 Grosvenor Blvd</p>
+              <p className='address-street'> </p>
+              <p className='address-street'> Los Angeles, CA</p>
+              <p className='address-street'> 90066, USA</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              3
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Nashville, TN
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>810 Crescent Centre Dr</p>
+              <p className='address-street'> Suite 530</p>
+              <p className='address-street'> Franklin, TN</p>
+              <p className='address-street'> 37067, USA</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              23
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            New York, NY
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>200 Varick St.</p>
+              <p className='address-street'> Suite 610</p>
+              <p className='address-street'> New York, NY</p>
+              <p className='address-street'> 10014, USA</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
               9
             </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            San Jose, CR
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>CM LATAM Metro Free Zone.</p>
+              <p className='address-street'> 5B-B</p>
+              <p className='address-street'> Barreal, CR</p>
+              <p className='address-street'> 40104, Costa Rica</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              23
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Sao Paulo, BR
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>270 R Henrique Schaumann</p>
+              <p className='address-street'> 6th Floor</p>
+              <p className='address-street'> Pinheiro, BR</p>
+              <p className='address-street'> CEP 05413-909, Brazil</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Tokyo, JP
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>Toun Building 13-10 Shibaura</p>
+              <p className='address-street'> 1-chome</p>
+              <p className='address-street'> Minato City, Tokyo, JP</p>
+              <p className='address-street'> 105-0023, Japan</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Toronto, ON
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>33 Bloor Street East</p>
+              <p className='address-street'> </p>
+              <p className='address-street'> Toronto, ON</p>
+              <p className='address-street'> M4W 3G8, Canada</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
+            <div className='location-opening'>
+              7
+            </div>
+            <div className='loaction-arrow'>
+              Jobs
+              <img src='./Assets/Arrow.svg' className='arrow' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='City-container'>
+        <div className='Seperator' />
+        <div className='City'>
+          <div className='City-name'>
+            Vancouver, BC
+          </div>
+          <div className='Address'>
+            <img src='./Assets/Calgary.webp' className='City-photo' />
+            <div className='Address-city'>
+              <p className='address-street'>BC V5H 2Y3, WeWork 6060</p>
+              <p className='address-street'>Silver Dr. Burnaby</p>
+              <p className='address-street'> 3rd</p>
+              <p className='address-street'> Vancouver, BC</p>
+              <p className='address-street'> V5H 2Y3, Canada</p>
+            </div>
+          </div>
+          <div className='location-jobs'>
             <div className='loaction-arrow'>
               Jobs
               <img src='./Assets/Arrow.svg' className='arrow' />
--- src/Components/Offices.jsx
+++ src/Components/Offices.jsx
@@ -5,7 +5,7 @@ import './Offices.css'
 function Offices() {
   return (
     <div className='Offices-div'>
-      <div className='Offices-title'>12 Officess.</div>
+      <div className='Offices-title'>12 Offices.</div>
       <div className='Offices-subheading'>We're all over the place.</div>
     </div>
   )
